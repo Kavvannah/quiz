517feabc1ab9d4b7aeb3648093df088bfdf93748
Kavvannah <103372963+Kavvannah@users.noreply.github.com>
2023-01-27 16:48:58 -0800

    :rocket:checking user answers using foreach method

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
-const correctAnswers = ['A', 'A', 'A', 'A', 'D' ];
+const correctAnswers = ['A', 'A', 'A', 'A', 'B' ];
 const form = document.querySelector('.quiz-form');
 
+// prevent refresh
 form.addEventListener('submit', e => {
     e.preventDefault();
 
     let score = 0;
     const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value];
+
+
+// check answers
+    userAnswers.forEach((answer, index) => {
+    if (answer === correctAnswers[index]) {
+        score += 20;
+    }
+    });
+
+    console.log(score);
 });
